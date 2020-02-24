c5c585026c3c4d6daca86cdd315c721a85542301
Mr.Ubay <61289643+mrubay@users.noreply.github.com>
2020-02-24 11:14:24 +0700

    Update tb.php

--- tb.php
+++ tb.php
@@ -173,11 +173,10 @@ echo color("blue"," ======================================\n");
          }
          }
          }
-         }
          }else{
          goto setpin;
          }
-         {else{
+         }else{
             echo color("red","-] Otp yang anda input salah");
             echo"\n==================================\n\n";
             echo color("yellow","!] Silahkan input kembali\n");
