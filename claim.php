933b0ea3eb4e5b7174a581329a79c9517faf013b
Mr.Ubay <61289643+mrubay@users.noreply.github.com>
2020-03-17 18:22:28 +0700

    Update claim.php

--- claim.php
+++ claim.php
@@ -90,7 +90,7 @@ echo "\e[89m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬�
         $uuid = getStr('"resource_owner_id":',',',$verif);
         echo color("white","+] Your access token : ".$token."\n\n");
         save("token.txt",$token);
-        echo color("green","\n▬▬▬▬▬▬▬▬▬▬▬▬▬🎫AUTO REDEEM VOUCHER🎫▬▬▬▬▬▬▬▬▬▬▬▬");
+        echo color("green","\n▬▬▬▬▬▬▬▬▬▬▬▬🎫AUTO REDEEM VOUCHER🎫▬▬▬▬▬▬▬▬▬▬▬▬");
         echo "\n".color("white","🔐▶️ Claim voc GORIDE 8K");
         echo "\n".color("yellow","⏳▶️ Please wait");
         for($a=1;$a<=3;$a++){
